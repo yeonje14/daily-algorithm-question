49ab7ab3dcfdfdb537e3b257a189b28a2f570ca1
yeonje14 <106527545+yeonje14@users.noreply.github.com>
2025-11-22 00:06:58 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int score;
+    scanf("%d", &score);
+    
+    if(score<=100 && score>=90)
+        printf("A");
+    else if(score<=89 && score>=80)
+        printf("B");
+    else if(score<=79 && score>=70)
+        printf("C");
+    else if(score<=69 && score>=60)
+        printf("D");
+    else
+        printf("F");
+    
+    return 0;
+}
