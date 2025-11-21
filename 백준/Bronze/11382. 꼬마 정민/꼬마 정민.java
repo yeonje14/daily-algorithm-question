0fb180af7566e6569897e5c53ce383f3eff889cc
yeonje14 <106527545+yeonje14@users.noreply.github.com>
2025-11-21 23:47:52 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner num = new Scanner(System.in);
+        long a = num.nextLong();
+        long b = num.nextLong();
+        long c = num.nextLong();
+  
+        System.out.println(a+b+c);
+    }
+}
