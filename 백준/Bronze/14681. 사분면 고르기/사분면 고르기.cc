1d1138687e2670aa851dfae9d06fa571789eecc2
yeonje14 <106527545+yeonje14@users.noreply.github.com>
2025-11-23 12:33:04 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int x, y, dump;
+	scanf("%d", &x);
+	scanf("%d", &y);
+	
+	if(x>0 && y>0)
+	{
+		printf("1");
+	}
+	else if(x<0 && y>0)
+	{
+		printf("2");
+	}
+	else if(x<0 && y<0)
+	{
+		printf("3");
+	}
+	else if(x>0 && y<0)
+	{
+		printf("4");
+	}
+	return 0;
+}
