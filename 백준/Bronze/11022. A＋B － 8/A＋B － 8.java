ab651ab2eef09f7d0ba67076629c60a06e1765c5
yeonje14 <106527545+yeonje14@users.noreply.github.com>
2025-12-01 17:45:41 +0900

    [Bronze V] Title: A+B - 8, Time: 216 ms, Memory: 19044 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        
+        for(int i = 1; i <= N; i++)
+        {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            System.out.println("Case #"+i+": "+A+" + "+B+" = "+(A+B));
+        }
+        scanner.close();
+    }
+}
