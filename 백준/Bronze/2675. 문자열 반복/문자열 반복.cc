ecae9af060bc1706155cc98dc66502e096146843
yeonje14 <106527545+yeonje14@users.noreply.github.com>
2025-12-12 22:56:00 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int T;
+    scanf("%d", &T);
+    
+    for(int i = 0; i < T; i++)
+    {
+        int R;
+        char S[21];
+        scanf("%d %s", &R, S);
+        for(int j = 0; S[j] != '\0'; j++)
+        {
+            for(int k = 0; k < R; k++)
+            {
+                printf("%c", S[j]);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
